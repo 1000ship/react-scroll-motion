9f7ff81d46742ffdcc2ce21facc6c26967d30523
1000ship <cjstjdgur1234@gmail.com>
2021-01-22 17:07:07 +0900

    chore: Version-up and remove dist from git

--- dist/ScrollContext.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ScrollPageContext = exports.ScrollContainerContext = void 0;
-var react_1 = require("react");
-exports.ScrollContainerContext = react_1.createContext({});
-exports.ScrollPageContext = react_1.createContext({ page: 0 });
--- dist/utils/interpolation.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SimpleInterpolation = void 0;
-var SimpleInterpolation = function (from, to, value) {
-    return from * (1 - value) + to * value;
-};
-exports.SimpleInterpolation = SimpleInterpolation;
