b36dfcc176ed6ca6097dc95261151e46855fd916
1000ship <cjstjdgur1234@gmail.com>
2021-01-30 19:09:08 +0900

    test: Make resize support

--- examples/src/react-scroll-motion/animations/StickyAnimation.ts
+++ examples/src/react-scroll-motion/animations/StickyAnimation.ts
@@ -1,56 +1,56 @@
 import environment from "../utils/environment";
 
-export const Sticky = (left:number = 50, top:number = 50) => ({
+export const Sticky = (left: number = 50, top: number = 50) => ({
   in: {
     style: {
-      left: `${left * environment.width / 100}px`,
-      top: `${top * environment.height / 100}px`,
+      left: () => `${(left * environment.width) / 100}px`,
+      top: () => `${(top * environment.height) / 100}px`,
       transform: "translate(-50%, -50%)",
       position: "fixed",
     },
   },
   out: {
     style: {
-      left: `${left * environment.width / 100}px`,
-      top: `${top * environment.height / 100}px`,
+      left: () => `${(left * environment.width) / 100}px`,
+      top: () => `${(top * environment.height) / 100}px`,
       transform: "translate(-50%, -50%)",
       position: "fixed",
     },
   },
 });
 
-export const StickyIn = (left:number = 50, top:number = 50) => ({
+export const StickyIn = (left: number = 50, top: number = 50) => ({
   in: {
     style: {
-      left: `${left * environment.width / 100}px`,
-      top: `${top * environment.height / 100}px`,
+      left: () => `${(left * environment.width) / 100}px`,
+      top: () => `${(top * environment.height) / 100}px`,
       transform: "translate(-50%, -50%)",
       position: "fixed",
     },
   },
   out: {
     style: {
-      left: `${left * environment.width / 100}px`,
-      top: `${top * environment.height / 100}px`,
+      left: () => `${(left * environment.width) / 100}px`,
+      top: () => `${(top * environment.height) / 100}px`,
       transform: "translate(-50%, -50%)",
       position: "absolute",
     },
   },
 });
 
-export const StickyOut = (left:number = 50, top:number = 50) => ({
+export const StickyOut = (left: number = 50, top: number = 50) => ({
   in: {
     style: {
-      left: `${left * environment.width / 100}px`,
-      top: `${top * environment.height / 100}px`,
+      left: `${(left * environment.width) / 100}px`,
+      top: `${(top * environment.height) / 100}px`,
       transform: "translate(-50%, -50%)",
       position: "absolute",
     },
   },
   out: {
     style: {
-      left: `${left * environment.width / 100}px`,
-      top: `${top * environment.height / 100}px`,
+      left: `${(left * environment.width) / 100}px`,
+      top: `${(top * environment.height) / 100}px`,
       transform: "translate(-50%, -50%)",
       position: "fixed",
     },
